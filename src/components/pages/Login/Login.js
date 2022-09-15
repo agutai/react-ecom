960b800ba45388e405ecacb420170ae0c7a987af
Joash Abejide <joashabejide@hotmail.co.uk>
2022-09-15 10:48:01 +0100

    Added concurrently, run-all, win-node-dev packages

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,66 +1,68 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Login.css';
-import Dashboard from "../Dashboard/Dashboard"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Login.css";
+import Dashboard from "../Dashboard/Dashboard";
 
 async function loginUser(credentials) {
-  return fetch('http://localhost:8081/login', {
-    method: 'POST',
+  return fetch("http://localhost:8081/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(credentials)
-  })
-    .then(data => data.json())
- }
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
 
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const token = await loginUser({
       username,
-      password
+      password,
     });
     setToken(token);
-  }
+  };
 
   function setToken(userToken) {
-    sessionStorage.setItem('token', JSON.stringify(userToken));
+    sessionStorage.setItem("token", JSON.stringify(userToken));
   }
 
   function getToken() {
-    const tokenString = sessionStorage.getItem('token');
+    const tokenString = sessionStorage.getItem("token");
     const userToken = JSON.parse(tokenString);
-    return userToken?.token
+    return userToken?.token;
   }
 
-  if(getToken()) {
-    return <Dashboard />
+  if (getToken()) {
+    return <Dashboard />;
   }
 
-  return(
+  return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)}/>
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)}/>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
 Login.propTypes = {
-  setToken: PropTypes.func.isRequired
+  setToken: PropTypes.func.isRequired,
 };
